e5c236219c6f652399691217e9831ea6cb9d513d
Umut YILDIRIM <umut475@gmail.com>
2024-03-16 16:55:26 -0700

    Update metadata and add og-image.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,49 @@ import Nav from './nav';
 import { Suspense } from 'react';
 
 export const metadata = {
-  title: 'Pricelytics',
-  description: 'Automatic AI Pricing for E-Commerce Products'
+  title: 'Pricelytics - AI-Based Pricing for E-Commerce',
+  description:
+    'Automate your e-commerce product pricing with our AI-driven solution, Pricelytics. Optimize prices for maximum profit and competitiveness.',
+  keywords: [
+    'AI',
+    'pricing',
+    'e-commerce',
+    'machine learning',
+    'product pricing'
+  ],
+  openGraph: {
+    title: 'Pricelytics - Automatic AI Pricing for E-Commerce Products',
+    description:
+      'Leverage AI to dynamically price your e-commerce products with Pricelytics, optimizing for market trends and maximizing profits.',
+    url: 'https://pricelytics.umutyildirim.com',
+    siteName: 'Pricelytics',
+    images: [
+      {
+        url: 'https://pricelytics.umutyildirim.com/og-image.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+        alt: 'Pricelytics Main Image'
+      },
+      {
+        url: 'https://pricelytics.umutyildirim.com/og-image.png', // Must be an absolute URL
+        width: 1200,
+        height: 630,
+        alt: 'Pricelytics Alternative Image'
+      }
+    ],
+    locale: 'en_US',
+    type: 'website'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Pricelytics - Automatic AI Pricing for E-Commerce Products',
+    description:
+      'Leverage AI to dynamically price your e-commerce products with Pricelytics, optimizing for market trends and maximizing profits.',
+    siteId: 'Pricelytics',
+    creator: '@pricelytics',
+    creatorId: 'Pricelytics',
+    images: ['https://pricelytics.umutyildirim.com/og-image.png'] // Must be an absolute URL
+  }
 };
 
 export default function RootLayout({
