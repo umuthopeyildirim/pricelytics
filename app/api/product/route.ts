8d45b1c600fe77966c9c90ddf97835806d7b1648
Umut YILDIRIM <umut475@gmail.com>
2024-03-17 11:07:12 -0700

    Refactor POST route to GET route in news API

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,16 +1,8 @@
 import { fetchProduct } from '../../lib/fetchProduct';
 
 export async function POST(request: Request) {
-  const url = new URL(request.url);
-  const id = url.searchParams.get('id');
-
-  if (!id) {
-    return new Response('Product id is missing', { status: 400 });
-  }
-
+  const res = await request.json();
+  const { id } = res;
   const product = await fetchProduct(id);
-  return new Response(JSON.stringify(product), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
+  return Response.json(product);
 }
